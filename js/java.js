ea4e6cc014666b01d8e472829ad8b66b0788f81e
EnzoCordeiro <105719804+EnzoCordeiro@users.noreply.github.com>
2022-05-18 12:33:12 -0300

    Add files via upload

--- js/java.js
+++ js/java.js
@@ -1,3 +1,19 @@
 function mostrartexto(){
     document.getElementById("botao").innerHTML = "Funciona!"
-}
+}var btn = document.querySelectorAll(" .key li")
+input = document.querySelector(".input- valor")
+operador = document.querySelectorAll(".operador")
+
+
+    //ciclo repetitivo intendificar se o usuario vai clicar em algum operador 
+    for(var = 0; i<btn.length; i+1) //lup que vai para o visor 
+     document.onkeypress = function(event){
+         var key = event.charCode;
+         //mudança para o navegador
+         //firefox interpretar calculadora
+           for(var e = 0; e <=10; e++){
+               if(key === (48+e)){
+                   input.innerHTML += e;
+               }
+           } 
+     }
